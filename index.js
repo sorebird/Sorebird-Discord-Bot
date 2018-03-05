eee69bf1cfa90b29ef6209609a94367b539a0723
sorebird <32424404+sorebird@users.noreply.github.com>
2018-03-05 19:28:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -515,8 +515,14 @@ bot.on('message', function(message) {
 		var selections = choice.split(',');
 		var n = selections.length;
 		var random = Math.round(Math.random() * (n - 1)) + 1;
-
-		message.channel.sendMessage('*chirp* o vo/ '+selections[random - 1].trim());
+		
+		var result = selections[random - 1].trim();
+		
+		if(result == "Vyrn"){
+			message.channel.sendFile('https://cdn.discordapp.com/attachments/356371808842416130/407520939505352714/Clipboard04.png');
+		}else{
+			message.channel.sendMessage('*chirp* o vo/ '+result);
+		}
 	}
 	
 });
