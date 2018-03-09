3b4fec7bf17f2b7c9454d1a75c1489be011415c5
sorebird <32424404+sorebird@users.noreply.github.com>
2018-03-09 20:03:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ bot.on('ready', () => {
 });
 
 bot.on('message', function(message) {
+    var channel = bot.channels.get("name", "general").id;
     var s = message.content;
     if(message.content == 'Sorebird'){
         message.channel.sendMessage('chirp');
@@ -628,6 +629,8 @@ bot.on('message', function(message) {
 	message.channel.sendFile('https://orig00.deviantart.net/bc55/f/2018/067/0/3/screenshot_20180208_145330_by_sorebird-dc5b2gd.jpg');
 	}else if(message.content == '$dab' || message.content == '$dablord'){
 	message.channel.sendFile('https://orig00.deviantart.net/2cae/f/2018/067/e/9/dablord_by_sena_nightsaber-dc5axhl.png');
+	}else if(message.content == '!getid'){
+	   message.channel.sendMessage('*chirp* o vo/ '+channel);
 	}
 	
 });
