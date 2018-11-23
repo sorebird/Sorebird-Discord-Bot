3ff6ff57fb5345e242e47ad1d0f6d45551186dcb
sorebird <32424404+sorebird@users.noreply.github.com>
2018-11-23 23:49:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -530,7 +530,7 @@ bot.on('message', function(message) {
 		
 		let modRole = message.member.roles.find("name",choice).id;
 		    
-		message.reply(modRole);
+		message.member.addRole(modRole);
 	}else if(message.content == '!chakchak'){
 	message.channel.sendFile('https://orig00.deviantart.net/a4e8/f/2018/067/e/5/1520534191283_by_sorebird-dc5b103.jpg');
 	}else if(message.content == '!KMRlol'){
