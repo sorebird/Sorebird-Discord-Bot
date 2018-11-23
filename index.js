670064146752e02beb98232808a43828d13d8466
sorebird <32424404+sorebird@users.noreply.github.com>
2018-11-23 23:58:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -528,7 +528,7 @@ bot.on('message', function(message) {
 		var choice = s.replace("!role", "");
 		choice = choice.trim();
 		
-		let modRole = message.member.roles.find("name",choice).id;
+		var modRole = message.member.roles.find("name",choice).id;
 		message.reply('*chirp* o vo/ '+modRole);
 	}else if(message.content == '!chakchak'){
 	message.channel.sendFile('https://orig00.deviantart.net/a4e8/f/2018/067/e/5/1520534191283_by_sorebird-dc5b103.jpg');
