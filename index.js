a0baae5c447ccf8593e62c4b5ecc2eb6042d8d6b
sorebird <32424404+sorebird@users.noreply.github.com>
2018-11-23 23:51:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -529,8 +529,7 @@ bot.on('message', function(message) {
 		choice = choice.trim();
 		
 		let modRole = message.member.roles.find("name",choice).id;
-		    
-		message.member.addRole(modRole);
+		message.reply('*chirp* o vo/ '+modRole);
 	}else if(message.content == '!chakchak'){
 	message.channel.sendFile('https://orig00.deviantart.net/a4e8/f/2018/067/e/5/1520534191283_by_sorebird-dc5b103.jpg');
 	}else if(message.content == '!KMRlol'){
