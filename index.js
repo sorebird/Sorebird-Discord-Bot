9ff3117a665a87178b710e7331cc028b6a983824
sorebird <32424404+sorebird@users.noreply.github.com>
2018-11-24 00:50:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -527,14 +527,14 @@ bot.on('message', function(message) {
 	}else if (String(s).match(/!addrole.*/)) {
 		var choice = s.replace("!addrole", "");
 		choice = choice.trim();
+		message.reply('*chirp* o vo/ '+choice);
 		
-		let modRole = message.guild.roles.find("name","Sorebird").id;
-		if(!modRole){
-		  message.reply('*chirp* o vo/ Role doesnt exist');
+		let modRole = message.guild.roles.find("name",choice);
+		
+		if(message.member.roles.has(modRole.id)){
+		  message.member.removeRole(modRole);
 		}else{
-		  if(!message.member.roles.has(modRole)){
-		    message.member.addRole(modRole);
-		  }
+		  message.member.addRole(modRole);	  
 		}
 	}else if(message.content == '!chakchak'){
 	message.channel.sendFile('https://orig00.deviantart.net/a4e8/f/2018/067/e/5/1520534191283_by_sorebird-dc5b103.jpg');
