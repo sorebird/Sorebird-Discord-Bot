d556e6f3842a564ae040168b9bac69bd76c27efc
sorebird <32424404+sorebird@users.noreply.github.com>
2019-11-22 02:59:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -535,7 +535,7 @@ bot.on('message', function(message) {
 			var s2 = selections[1];
 			var s3 = selections[2] * 10;
 		
-			var result = math.sum(s1,s2,s3);
+			var result = Math.sum(s1,s2,s3);
 		
 			if(isNaN(result)){
 				message.reply('o v o please use this format "!spark set crystals,single ticket,10-draw"');
