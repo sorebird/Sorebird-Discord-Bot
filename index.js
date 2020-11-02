31c2ee323af5044fbeede381ee0f9ccb439beb73
sorebird <32424404+sorebird@users.noreply.github.com>
2020-11-02 21:57:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1376,12 +1376,12 @@ bot.on('message', message => {
 		}
 		
 		const avatarList = message.mentions.users.map(user => {
-			return `${user.username}'s avatar: <${user.displayAvatarURL({ format: "png", dynamic: true })}>`;
+			return `<${user.displayAvatarURL({ format: "png", dynamic: true })}>`;
 		});
 
 		// send the entire array of strings as a message
 		// by default, discord.js will `.join()` the array with `\n`
-		message.channel.send(avatarList);
+		message.channel.send({files: [avatarList]});
 	}
 });
 
